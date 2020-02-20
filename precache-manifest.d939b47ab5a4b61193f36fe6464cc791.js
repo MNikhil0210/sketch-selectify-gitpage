45c918302c3db4fbb01ff42f836dbf004f366b01
Nikhil-Intruder <nikhilpavansai.machavaram@zemosolabs.com>
2020-02-20 11:06:27 +0530

    Updates

---
+++ precache-manifest.d939b47ab5a4b61193f36fe6464cc791.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1210a8ae21444aefb35b5ad4e4571f27",
+    "url": "/sketch-selectify-gitpage/index.html"
+  },
+  {
+    "revision": "be0dc96f9a0bf4662db7",
+    "url": "/sketch-selectify-gitpage/static/css/main.032e467b.chunk.css"
+  },
+  {
+    "revision": "43cb9b7875b562c0c5e9",
+    "url": "/sketch-selectify-gitpage/static/js/2.04b1f7b5.chunk.js"
+  },
+  {
+    "revision": "aa72db0bfab74e637ca50275ccc280a2",
+    "url": "/sketch-selectify-gitpage/static/js/2.04b1f7b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be0dc96f9a0bf4662db7",
+    "url": "/sketch-selectify-gitpage/static/js/main.a9374411.chunk.js"
+  },
+  {
+    "revision": "496422c49182ee7ac304",
+    "url": "/sketch-selectify-gitpage/static/js/runtime-main.0aafe8e0.js"
+  },
+  {
+    "revision": "e56342e57c91bceda9fe3269c4107d08",
+    "url": "/sketch-selectify-gitpage/static/media/tool1.e56342e5.svg"
+  },
+  {
+    "revision": "fb325b82dc44336ce8a77a0f3ce912ac",
+    "url": "/sketch-selectify-gitpage/static/media/tool2.fb325b82.svg"
+  },
+  {
+    "revision": "7fa4b58bdd168b1f6013d5c9f9f0b776",
+    "url": "/sketch-selectify-gitpage/static/media/tool3.7fa4b58b.svg"
+  },
+  {
+    "revision": "b1fb1d7a7bc9387368666386b88ed3da",
+    "url": "/sketch-selectify-gitpage/static/media/zemoso.b1fb1d7a.svg"
+  }
+]);
